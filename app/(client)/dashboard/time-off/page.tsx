318797818d855d48c4e0839b49f166226743f5dd
ry-arrixa <156094512+ry-arrixa@users.noreply.github.com>
2024-03-14 13:50:06 +0000

    New Sidebar Recommendation

--- app/(client)/dashboard/time-off/page.tsx
+++ app/(client)/dashboard/time-off/page.tsx
@@ -13,7 +13,6 @@ const TimeoffPage = () => {
         <main className="flex flex-col items-left w-full lg:p-10 md:p-6 p-2">
         <h1 className="text-3xl text-left pl-6 lg:px-10 md:px-10 font-semibold my-4 pt-4">Time Off</h1>
         <h3 className="text-lg text-left pl-6 lg:px-10 md:px-10 my-4">Request and Track your leave request.</h3>
-        {/* <TimeTracker /> */}
         <Tabs defaultValue="calendar" className="w-full">
                 <TabsList>
                     <TabsTrigger value="calendar">Calendar</TabsTrigger>
--- lib/sidebarItemsConst.ts
+++ lib/sidebarItemsConst.ts
@@ -1,10 +1,40 @@
 export const sidebarItems = [
   { role: "ADMIN", label: "Admin", href: "/dashboard/admin" },
   { role: "EMPLOYEE", label: "Time tracker", href: "/dashboard/time" },
+  { role: "EMPLOYEE", label: "Time off", href: "/dashboard/time-off" },
   { role: "EMPLOYEE", label: "Time report", href: "/dashboard/time-report" },
   { role: "EMPLOYEE", label: "Expense tracker", href: "/dashboard/expense" },
   { role: "EMPLOYEE", label: "Inbox", href: "#" },
   { role: "ADMIN", label: "Users", href: "#" },
   { role: "EMPLOYEE", label: "Preferences", href: "#" },
   { role: "EMPLOYEE", label: "Settings", href: "#" },
+];
+
+export const newSidebarItems = [
+  {
+    role: "ADMIN",
+    menuItems: [
+      { label: "Admin", href: "/dashboard/admin" },
+      { label: "Users", href: "#" },
+      { label: "Time tracker", href: "/dashboard/time" },
+      { label: "Time off", href: "/dashboard/time-off" },
+      { label: "Time report", href: "/dashboard/time-report" },
+      { label: "Expense tracker", href: "/dashboard/expense" },
+      { label: "Inbox", href: "#" },
+      { label: "Preferences", href: "#" },
+      { label: "Settings", href: "#" },
+    ],
+  },
+  {
+    role: "EMPLOYEE",
+    menuItems: [
+      { label: "Time tracker", href: "/dashboard/time" },
+      { label: "Time off", href: "/dashboard/time-off" },
+      { label: "Time report", href: "/dashboard/time-report" },
+      { label: "Expense tracker", href: "/dashboard/expense" },
+      { label: "Inbox", href: "#" },
+      { label: "Preferences", href: "#" },
+      { label: "Settings", href: "#" },
+    ],
+  },
 ];
